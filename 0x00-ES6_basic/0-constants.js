f30041fa42fc1d178e219c04c0f774a0b05bf48c
joeldavid <daveside00468@gmail.com>
2023-04-17 11:19:07 +0000

    my first commit

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+function taskFirst() {
+  const name = 'John';
+  const age = 32;
+  console.log(`My name is ${name} and I am ${age} years old.`);
+}
+
+function taskNext() {
+  let counter = 0;
+  console.log(counter);
+  counter++;
+}
+
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,1 @@
+my readme file content for 0x00-ES6_basic
