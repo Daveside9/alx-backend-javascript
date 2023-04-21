dbfaddaa7d8bbee44471b206e9463dbbfe039193
joeldavid <daveside00468@gmail.com>
2023-04-21 13:42:47 -0700

    Test

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
-	let employeeNames = [];
+  const employeeNames = [];
 
-	// iterate through each employee name using the iterator object
-	for (let name of reportWithIterator) {
-		employeeNames.push(name); 
-	}
+  // iterate through each employee name using the iterator object
+  for (const name of reportWithIterator) {
+    employeeNames.push(name);
+  }
 
-	// join the array of employee names into a string separated by |
-	return employeeNames.join(' | ');
+  // join the array of employee names into a string separated by |
+  return employeeNames.join(' | ');
 }
