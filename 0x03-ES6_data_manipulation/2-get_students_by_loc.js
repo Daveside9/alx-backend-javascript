2361dc1591ef3a2ac1a07345a6a8894b88a70ff8
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-05-08 03:42:02 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,5 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
+import getListStudents from './0-get_list_students';
 
-const students = getListStudents();
-const sfStudents = getStudentsByLocation(students, 'San Francisco');
-
-console.log(sfStudents);
+export default function getStudentsByLocation(student = getListStudents(), city) {
+  return student.filter((el) => el.location === city);
+}
