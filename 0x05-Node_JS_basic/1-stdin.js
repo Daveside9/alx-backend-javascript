9439410a9d137c0fb3dff3954085f8c84a82b57f
joeldavid <daveside00468@gmail.com>
2023-06-11 15:45:32 -0700

    Test

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const input = data.trim();
+
+  if (input) {
+    console.log(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('\nThis important software is now closing');
+});
+
