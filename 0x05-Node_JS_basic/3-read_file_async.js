2c297dad9bd15ab17e5ce861a4178c6e5c157b5e
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-06-14 02:58:01 -0700

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,42 @@
-const fs = require('fs');
+const { readFile } = require('fs');
 
-function countStudents(path) {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
+    readFile(fileName, (error, data) => {
       if (error) {
-        reject(new Error('Cannot load the database'));
+        reject(Error('Cannot load the database'));
       } else {
-        const lines = data.split('\n').filter((line) => line !== '');
-
-        const students = {
-          CS: [],
-          SWE: [],
-        };
-
-        lines.forEach((line) => {
-          const [firstName, , , field] = line.split(',');
-
-          if (field === 'CS') {
-            students.CS.push(firstName);
-          } else if (field === 'SWE') {
-            students.SWE.push(firstName);
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
           }
-        });
-
-        console.log(`Number of students: ${lines.length}`);
-        console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
-        console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
-
-        resolve();
+        }
+        const l = length - 1;
+        console.log(`Number of students: ${l}`);
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+          }
+        }
+        resolve(data);
       }
     });
   });
 }
 
 module.exports = countStudents;
-
