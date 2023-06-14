112bebe936283aec13af8f5a3e224cb29b3f9aef
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-06-14 02:50:51 -0700

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,3 @@
-const fs = require('fs');
+const countStudents = require('./2-read_file');
 
-function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-
-    console.log(`Number of students: ${lines.length - 1}`);
-
-    const fields = lines[0].split(',');
-
-    fields.forEach((field, index) => {
-      if (index > 0) {
-        const students = [];
-        lines.slice(1).forEach(line => {
-          const values = line.split(',');
-          students.push(values[index]);
-        });
-
-        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-      }
-    });
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
-
-module.exports = countStudents;
-
+countStudents("database.csv");
