42198df21922cfd0d9e80d2b66e889c80e75e12f
joeldavid <daveside00468@gmail.com>
2023-08-22 15:58:51 -0700

    my commit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(message) {
-    console.log(message);
-}
-
-module.export = displayMessage;
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author joel david <https://github.com/Daveside9>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
